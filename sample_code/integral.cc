de3d4486fc426719090c141ff7d8aed151da0988
Hsiang-Yin Hsieh <hhsie011@ucr.edu>
2021-01-22 04:29:53 -0800

    implemented using cilk for loop

--- sample_code/integral.cc
+++ sample_code/integral.cc
@@ -3,12 +3,22 @@
 #include <math.h>
 #include "get_time.h"
 
+// long double getIntegral(int, int n) {
+//     if (n == 1) {
+//         return (sin(1 + ))
+//     }
+//     left_integral = cilk_spawn getIntegral();
+//     right_integral = getIntegral();
+//     return left_integral + right_integral;
+// }
+
 int main() {
     int segs = 10^8;
     long double sum = 0;
     timer t;
-    for (int i = 0; i < segs; ++i) {
-        sum += (sin(1 + i / segs) * log(1 + i / segs) + sin(1 + (i + 1) / segs) * log(1 + (i + 1) / segs)) / segs / 2;
+    // sum = getIntegral();
+    cilk_for (int i = 0; i < segs; i++) {
+        sum += (sin(1 + i / segs) * log(1 + i  segs) + sin(1 + (i + 1) / segs) * log(1 + (i + 1) / segs) / segs / 2);
     }
     t.stop();
     std::cout << "sum: " << sum << std::endl;
